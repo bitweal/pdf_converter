783e5fdd4b01066b768df67ee67a9b08b34b14e0
アンドリュー <bitweal12@gmail.com>
2024-05-18 14:31:39 +0300

    Added split_pdf()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfMerger, PdfReader, PdfWriter
 
 
 def merge_pdfs(pdf_list, output):
@@ -9,6 +9,16 @@ def merge_pdfs(pdf_list, output):
     merger.close()
 
 
+def split_pdf(input_pdf, start_page, end_page, output_pdf):
+    reader = PdfReader(input_pdf)
+    writer = PdfWriter()
+    for page in range(start_page, end_page):
+        writer.add_page(reader.pages[page])
+    with open(output_pdf, 'wb') as output:
+        writer.write(output)
+
+
 if __name__ == '__main__':
     pdf_file = 'media/iasa-open_21_.pdf'
-    merge_pdfs([pdf_file, pdf_file, pdf_file], 'merge_pdfs.pdf')
+    #merge_pdfs([pdf_file, pdf_file, pdf_file], 'media/merge_pdfs.pdf')
+    #split_pdf('media/merge_pdfs.pdf', 15, 42, 'media/split.pdf')
