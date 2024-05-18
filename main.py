48f8b731324bb7f220b83d80251e08f7e0561311
アンドリュー <bitweal12@gmail.com>
2024-05-18 15:51:21 +0300

    Added compress_pdf()

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PyPDF2 import PdfMerger, PdfReader, PdfWriter
+import fitz
+import os
 
 
 def merge_pdfs(pdf_list, output):
@@ -18,7 +20,27 @@ def split_pdf(input_pdf, start_page, end_page, output_pdf):
         writer.write(output)
 
 
+def compress_pdf(input_pdf, output_pdf, dpi=100):
+    doc = fitz.open(input_pdf)
+    new_doc = fitz.open()
+
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        rect = page.rect
+        pix = page.get_pixmap(dpi=dpi)
+        image_path = f"media/temp_page_{page_num}.jpg"
+        pix.save(image_path)
+        new_page = new_doc.new_page(width=rect.width, height=rect.height)
+        new_page.insert_image(rect, filename=image_path)
+        os.remove(image_path)
+
+    new_doc.save(output_pdf, garbage=4, deflate=True)
+    new_doc.close()
+    doc.close()
+
+
 if __name__ == '__main__':
     pdf_file = 'media/iasa-open_21_.pdf'
-    #merge_pdfs([pdf_file, pdf_file, pdf_file], 'media/merge_pdfs.pdf')
+    #merge_pdfs([pdf_file for _ in range(100)], 'media/merge_pdfs.pdf')
     #split_pdf('media/merge_pdfs.pdf', 15, 42, 'media/split.pdf')
+    compress_pdf('media/merge_pdfs.pdf', 'media/compress_pdf.pdf', 100)
